99db587591d43a6c8d02e11b198b88f0c9586d49
skyGaneshram <ganeshram.Sampath.ciec@skytv.it>
2025-03-07 13:34:59 +0530

    Integrating frontend and backend

---
+++ src/main/java/streakflix/configs/StaticView.java
@@ -0,0 +1,16 @@
+package streakflix.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticView implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward all unmatched routes to index.html
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+}
